3d5b2cc23552561a84fde531f0471168344ed046
hopkins385 <stadhouders.sven@gmail.com>
2025-01-04 19:28:52 +0100

    [breaking] init migrations

--- prisma/migrations/20250104182632_init/migration.sql
+++ prisma/migrations/20250104182632_init/migration.sql
@@ -10,13 +10,13 @@ CREATE TABLE "users" (
     "device_id" TEXT,
     "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "image" TEXT,
     "first_name" TEXT,
     "last_name" TEXT,
     "password" TEXT,
     "is_active" BOOLEAN NOT NULL DEFAULT false,
     "last_login_at" TIMESTAMP(3),
-    "email_verified" TIMESTAMP(3),
-    "image" TEXT,
+    "email_verified_at" TIMESTAMP(3),
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
     "deleted_at" TIMESTAMP(3),
@@ -203,6 +203,47 @@ CREATE TABLE "assistant_tools" (
     "deleted_at" TIMESTAMP(3),
 
     CONSTRAINT "assistant_tools_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "assistant_templates" (
+    "id" TEXT NOT NULL,
+    "llm_id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "system_prompt" TEXT NOT NULL,
+    "system_prompt_token_count" INTEGER NOT NULL,
+    "config" JSON,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "assistant_templates_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "assistant_template_categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "config" JSON,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "assistant_template_categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "assistant_template_category_items" (
+    "id" TEXT NOT NULL,
+    "category_id" TEXT NOT NULL,
+    "template_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "assistant_template_category_items_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -641,6 +682,15 @@ ALTER TABLE "assistant_tools" ADD CONSTRAINT "assistant_tools_assistant_id_fkey"
 
 -- AddForeignKey
 ALTER TABLE "assistant_tools" ADD CONSTRAINT "assistant_tools_tool_id_fkey" FOREIGN KEY ("tool_id") REFERENCES "tools"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "assistant_templates" ADD CONSTRAINT "assistant_templates_llm_id_fkey" FOREIGN KEY ("llm_id") REFERENCES "llms"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "assistant_template_category_items" ADD CONSTRAINT "assistant_template_category_items_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "assistant_template_categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "assistant_template_category_items" ADD CONSTRAINT "assistant_template_category_items_template_id_fkey" FOREIGN KEY ("template_id") REFERENCES "assistant_templates"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "credits" ADD CONSTRAINT "credits_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
