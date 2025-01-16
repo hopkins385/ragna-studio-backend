ed4c5f189e3c0897708a8d0a7bbf29ec88b6657d
hopkins385 <stadhouders.sven@gmail.com>
2025-01-16 11:15:48 +0100

    infinite sub categories

--- prisma/seed/assistant-templates.seed.ts
+++ prisma/seed/assistant-templates.seed.ts
@@ -2,7 +2,7 @@ import { SeedPrismaClient, prismaSeedClient } from './seed.config';
 import { faker } from '@faker-js/faker';
 
 async function seedAssistantTemplateCategories(prisma: SeedPrismaClient) {
-  return prisma.assistantTemplateCategory.createManyAndReturn({
+  const cats = await prisma.assistantTemplateCategory.createManyAndReturn({
     data: [
       {
         name: 'template.category.recommended.title',
@@ -66,6 +66,94 @@ async function seedAssistantTemplateCategories(prisma: SeedPrismaClient) {
       },
     ],
   });
+
+  /*const subcats = await prisma.assistantTemplateCategory.createManyAndReturn({
+    data: [
+      {
+        name: 'template.category.recommended.subcategory.title',
+        description: 'template.category.recommended.subcategory.description',
+        config: {
+          icon: 'star',
+          color: 'blue',
+        },
+        parentId: cats.find(
+          (c) => c.name === 'template.category.recommended.title',
+        ).id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'template.category.business.subcategory.title',
+        description: 'template.category.business.subcategory.description',
+        config: {
+          icon: 'briefcase',
+          color: 'green',
+        },
+        parentId: cats.find(
+          (c) => c.name === 'template.category.business.title',
+        ).id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'template.category.marketing.subcategory.title',
+        description: 'template.category.marketing.subcategory.description',
+        config: {
+          icon: 'megaphone',
+          color: 'red',
+        },
+        parentId: cats.find(
+          (c) => c.name === 'template.category.marketing.title',
+        ).id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'template.category.social_media.subcategory.title',
+        description: 'template.category.social_media.subcategory.description',
+        config: {
+          icon: 'share',
+          color: 'yellow',
+        },
+        parentId: cats.find(
+          (c) => c.name === 'template.category.social_media.title',
+        ).id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'template.category.education.subcategory.title',
+        description: 'template.category.education.subcategory.description',
+        config: {
+          icon: 'graduation',
+          color: 'purple',
+        },
+        parentId: cats.find(
+          (c) => c.name === 'template.category.education.title',
+        ).id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'template.category.personal.subcategory.title',
+        description: 'template.category.personal.subcategory.description',
+        config: {
+          icon: 'user',
+          color: 'orange',
+        },
+        parentId: cats.find(
+          (c) => c.name === 'template.category.personal.title',
+        ).id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ],
+  });
+
+  return cats.concat(subcats);
+  */
+
+  return cats;
 }
 
 async function seedAssistantTemplates(prisma: SeedPrismaClient) {
@@ -82,8 +170,9 @@ async function seedAssistantTemplates(prisma: SeedPrismaClient) {
   return prisma.assistantTemplate.createManyAndReturn({
     data: Array.from({ length: 100 }).map(() => ({
       llmId: llms[Math.floor(Math.random() * llmsCount)].id,
-      title: faker.lorem.words(),
-      description: faker.lorem.sentence(),
+      title: 'Text zusammen\u00ADfassen',
+      description:
+        'Vorgegebener Text wird in einen kurzen Text zusammengefasst.',
       systemPrompt: {
         de: faker.lorem.sentence(),
         en: faker.lorem.sentence(),
@@ -91,6 +180,7 @@ async function seedAssistantTemplates(prisma: SeedPrismaClient) {
       config: {
         icon: icons[Math.floor(Math.random() * icons.length)],
         color: faker.color.human(),
+        free: faker.datatype.boolean(),
       },
       createdAt: new Date(),
       updatedAt: new Date(),
