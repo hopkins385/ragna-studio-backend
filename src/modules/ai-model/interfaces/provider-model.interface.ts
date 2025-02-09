74475fba42f0f6e7acaa07e4263d5e7014302488
hopkins385 <stadhouders.sven@gmail.com>
2025-02-09 15:53:12 +0100

    fix typing

--- src/modules/ai-model/interfaces/provider-model.interface.ts
+++ src/modules/ai-model/interfaces/provider-model.interface.ts
@@ -1,12 +1,13 @@
 import { ConfigService } from '@nestjs/config';
 import { ProviderType } from '../enums/provider.enum';
 import { AiModelProviderOptions } from '../schemas/aiModelProvider';
+import { LanguageModelV1 } from 'ai';
 
 export type ProviderClass = new (
   model: string,
   config: ConfigService,
   options: AiModelProviderOptions,
-) => { createModel(): any };
+) => { createModel(): LanguageModelV1 };
 
 export interface ProviderModelConfig {
   provider: ProviderType;
