b96c6fb61d07f362a361f656304a61da2f3a9964
hopkins385 <stadhouders.sven@gmail.com>
2025-02-20 16:40:21 +0100

    cleanup

--- src/modules/chat/repositories/chat.repository.ts
+++ src/modules/chat/repositories/chat.repository.ts
@@ -1,7 +1,5 @@
-import { BaseRepository } from '@/common/types/repository.types';
 import { ExtendedPrismaClient } from '@/modules/database/prisma.extension';
 import { Inject, Injectable } from '@nestjs/common';
-import { Chat } from '@prisma/client';
 import { CustomPrismaService } from 'nestjs-prisma';
 
 @Injectable()
