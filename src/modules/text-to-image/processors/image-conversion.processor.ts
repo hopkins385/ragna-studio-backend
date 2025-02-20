fe12e56dc84cd4f7ede3ce51919a5573425c31a6
hopkins385 <stadhouders.sven@gmail.com>
2025-02-20 10:58:22 +0100

    enable worker

--- src/modules/text-to-image/processors/image-conversion.processor.ts
+++ src/modules/text-to-image/processors/image-conversion.processor.ts
@@ -35,7 +35,7 @@ export class ImageConversionProcessor extends WorkerHost {
     private readonly httpClient: AxiosInstance,
   ) {
     super();
-    // this.imageWorker = this.createWorker();
+    this.imageWorker = this.createWorker();
   }
 
   async process(job: Job<any, any, string>): Promise<any> {
